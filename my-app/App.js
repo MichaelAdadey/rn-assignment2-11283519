8e7945d1c61b2b9634666197dcf02d9e1e3286fe
michaeladadey360 <michaeladadey360@gmail.com>
2024-05-27 15:53:56 +0000

    Add app.js

---
+++ my-app/App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        My name is <Text style={styles.boldText}>Michael Adadey</Text>
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#3d3d3d',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+  text: {
+    fontSize: 24,
+  },
+
+  boldText: {
+    fontWeight: 'bold',
+  },
+});
---
+++ my-app/readMe.md
@@ -0,0 +1,6 @@
+![screenshot](<localhost_8081_(Surface Duo).png>)
+
+DESCRIPTION:
+This React Native code defines a simple mobile application that displays a text message on the screen. The message "My name is Michael Adadey" is shown, with "Michael Adadey" in bold. The app uses View and Text components from React Native to structure and style the content. The StatusBar component is used to manage the status bar appearance.
+
+Michael Adadey - 11283519
